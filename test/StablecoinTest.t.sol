67948c15f5595a120fce17e87bee4c33c3010d9e
Vector <vector02ozo@gmail.com>
2025-04-11 16:42:52 +0100

    test commit

--- test/StablecoinTest.t.sol
+++ test/StablecoinTest.t.sol
@@ -3,11 +3,16 @@
 pragma solidity ^0.8.20;
 
 import {Test, console} from "forge-std/Test.sol";
-import {Stablecoin} from "../src/Stablecoin.sol"; 
+import {Stablecoin} from "../src/Stablecoin.sol";
 import {DeployStablecoin} from "../script/DeployStablecoin.s.sol";
 
 contract StablecoinTest is Test {
+    Stablecoin stablecoin;
+    DeployStablecoin deployStablecoin;
+
     function setUp() public {
-        
+        vm.startBroadcast();
+        stablecoin = new Stablecoin();
+        vm.stopBroadcast();
     }
 }
