4f270e02b0682abfbbfdad393f0453a5997d0137
Vector <vector02ozo@gmail.com>
2025-04-12 22:02:08 +0100

    test commit

--- script/DeployStablecoin.s.sol
+++ script/DeployStablecoin.s.sol
@@ -3,12 +3,36 @@ pragma solidity ^0.8.18;
 
 import {Stablecoin} from "../src/Stablecoin.sol";
 import {Script, console} from "forge-std/Script.sol";
+import {HelperConfig} from "../script/HelperConfig.s.sol";
+import {StablecoinEngine} from "../src/StablecoinEngine.sol";
 
 contract DeployStablecoin is Script {
-    function run() external {
-        vm.startBroadcast();
+    address[] public tokenAddresses;
+    address[] public priceFeedAddresses;
+
+    function run() public returns (Stablecoin, StablecoinEngine, HelperConfig) {
+        HelperConfig config = new HelperConfig();
+        (
+            address ethUsdPriceFeed,
+            address btcUsdPriceFeed,
+            address weth,
+            address wbtc,
+            uint256 deployerKey
+        ) = config.activeNetworkConfig();
+
+        tokenAddresses = [weth, wbtc];
+        priceFeedAddresses = [ethUsdPriceFeed, btcUsdPriceFeed];
+
+        vm.startBroadcast(deployerKey);
         Stablecoin stablecoin = new Stablecoin();
-        console.log("Stablecoin deployed to: ", address(stablecoin));
+        StablecoinEngine engine = new StablecoinEngine(
+            tokenAddresses,
+            priceFeedAddresses,
+            address(stablecoin)
+        );
+        stablecoin.transferOwnership(address(engine));
         vm.stopBroadcast();
+
+        return (stablecoin, engine, config);
     }
 }
--- test/StablecoinTest.t.sol
+++ test/StablecoinTest.t.sol
@@ -5,14 +5,35 @@ pragma solidity ^0.8.20;
 import {Test, console} from "forge-std/Test.sol";
 import {Stablecoin} from "../src/Stablecoin.sol";
 import {DeployStablecoin} from "../script/DeployStablecoin.s.sol";
+import {HelperConfig} from "../script/HelperConfig.s.sol";
+import {StablecoinEngine} from "../src/StablecoinEngine.sol";
 
 contract StablecoinTest is Test {
     Stablecoin stablecoin;
     DeployStablecoin deployStablecoin;
+    HelperConfig config;
+    StablecoinEngine engine;
+    address ethUsdPriceFeed;
+    address weth;
 
     function setUp() public {
-        vm.startBroadcast();
-        stablecoin = new Stablecoin();
-        vm.stopBroadcast();
+        deployStablecoin = new DeployStablecoin();
+        (stablecoin, engine, config) = deployStablecoin.run();
+        (ethUsdPriceFeed, , weth, , ) = config.activeNetworkConfig();
+        vm.label(ethUsdPriceFeed, "ETH/USD Price Feed");
+        vm.label(weth, "WETH");
+    }
+
+    /////////////////////
+    //// CORE TESTS ////
+    ///////////////////
+
+    function testGetUsdValue() public {
+        uint256 ethAmount = 5e18; // 5ETH
+        // 5 * 2000 = 10000 USD
+        uint256 expectedValue = 10000e18; // 10000 USD
+
+        uint256 actualValue = engine.getTokenInUsd(weth, ethAmount);
+        assertEq(expectedValue, actualValue);
     }
 }
